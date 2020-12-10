7a9f1d000afdb1d37ce7dad0ad6283e83b293989
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-12-09 23:31:08 -0300

    feat: add Button.jsx, remove storybook.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Container from "./StyledComponents";
 
-const Button = props => {
+const Button = ({ title, width, height, background }) => {
   return (
-    <div>
-      <button>teste</button>
-    </div>
+    <Container width={width} height={height} background={background}>
+      <h1>{title}</h1>
+    </Container>
   )
 }
 
